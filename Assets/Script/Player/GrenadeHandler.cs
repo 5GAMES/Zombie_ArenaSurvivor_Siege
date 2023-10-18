80991fbb636475437054b0ee403089ea685a3971
Master_Shadows <pozitivmr15@gmail.com>
2023-10-18 22:07:31 +0300

    0.21b

--- Assets/Script/Player/GrenadeHandler.cs
+++ Assets/Script/Player/GrenadeHandler.cs
@@ -1,15 +1,21 @@
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GrenadeHandler : MonoBehaviour
 {
     [SerializeField] private Grenade grenadePrefab;
     [SerializeField] private GameObject _grenadeThrowPoint;
-    [SerializeField]private int _genadeCout = 1;
-    [SerializeField] private int _maxGrenade;
+    [SerializeField] private int _genadeCout = 2;
+    [SerializeField] private int _maxGrenade = 5;
+    [SerializeField] TextMeshProUGUI _textCoutGrenade;
     public int MaxGrenadeCount { get { return _maxGrenade; } }
     private bool _onCooldown = false;
-    
+    private void Start()
+    {
+       _textCoutGrenade.text = _genadeCout.ToString();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
@@ -20,22 +26,30 @@ public class GrenadeHandler : MonoBehaviour
             }
         }
     }
-
     private async void Throw()
     {
+
         if (_onCooldown) return;
         _genadeCout--;
+        _textCoutGrenade.text = _genadeCout.ToString();
         _onCooldown = true;
         Grenade grenade = Instantiate(grenadePrefab, _grenadeThrowPoint.transform.position, Quaternion.identity);
         grenade.Throw(transform.forward);
         await Task.Delay(5000);
         _onCooldown = false;
+       
     }
-
     public int AddGrenade()
     {
-        if(_genadeCout + 1 <= _maxGrenade) _genadeCout++;
+        if(_genadeCout <= _maxGrenade)
+        {
+            _genadeCout++;
+            _textCoutGrenade.text = _genadeCout.ToString();
+        }
         return _genadeCout;
+        
+        
+        
     }
 
 
