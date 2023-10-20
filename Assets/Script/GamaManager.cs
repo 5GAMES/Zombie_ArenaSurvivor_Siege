6d63ba1508ec91c8f4d3f4a28b883731706bf262
Master_Shadows <pozitivmr15@gmail.com>
2023-10-20 17:22:54 +0300

    Auto stash before merge of "main" and "origin/main"

--- Assets/Script/GamaManager.cs
+++ Assets/Script/GamaManager.cs
@@ -1,8 +1,11 @@
 
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
+using YG;
 
 public class GamaManager : MonoBehaviour
 {
@@ -10,32 +13,79 @@ public class GamaManager : MonoBehaviour
     [SerializeField] private Volume _volume;
     [SerializeField] private DepthOfField _depthOfField;
     [SerializeField] private ShopItem _shopItem;
+    [Header("ADD")]
+    [SerializeField] private Slider _sliderFullScreen;
+    [SerializeField] private YandexGame _sdk;
+    private float _time = 1f;
+    private float _timer = 10f;
+    [SerializeField] bool _IsAdplay;
+    [SerializeField] private TextMeshProUGUI _textad;
+    [SerializeField] private GameObject _fullsceenAd;
+
+    [Header("Authentication")]
+    [SerializeField] private GameObject _authentication;
     public Image Icones;
     void Start()
     {
+        _sliderFullScreen.minValue = 0f;
+        _sliderFullScreen.maxValue = _timer;
+        _sliderFullScreen.value = 0f;
         if (!_volume.profile.TryGet(out _depthOfField))
         {
             Debug.LogError("Depth of Field is not set up in the Volume");
         }
+        StartCoroutine(AdFullScreen());
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.M))
-        {
-            
-        }
+       
+        AddSdk();
     }
     public void ResumeGames() => _menu.PauseGames();
 
     public void Boken()
     {
-        if(!_depthOfField.active)
+        if (!_depthOfField.active) _depthOfField.active = true;
+        else _depthOfField.active = false;
+    }
+    public void AddSdk()
+    {
+        if (_IsAdplay == true)
         {
-            _depthOfField.active = true;
+            int roundedTimer = Mathf.FloorToInt(_timer);
+            if (roundedTimer >= 0)
+            {
+                _timer -= Time.deltaTime;
+                _textad.text = roundedTimer.ToString();
+            }
+            _sliderFullScreen.value += Time.deltaTime;
+            if (_sliderFullScreen.value == 10)
+            {
+                _IsAdplay = false;
+                _fullsceenAd.SetActive(false);
+                _sdk._FullscreenShow();
+
+            }
         }
-        else
-        {
-            _depthOfField.active = false;
-        }
+    }
+    private IEnumerator AdFullScreen()
+    {
+        yield return new WaitForSeconds(60f);
+        _IsAdplay = true;
+        _fullsceenAd.SetActive(true);
+    }
+    public void NoAuthentication()
+    {
+        StartCoroutine(NoAuthenticationCoroutine());
+    }
+    private IEnumerator NoAuthenticationCoroutine()
+    {
+        _authentication.SetActive(true);
+        yield return new WaitForSeconds(5);
+        _authentication.SetActive(false);
+    }
+    public void AddMoneySdk()
+    {
+        _sdk._RewardedShow(1);
     }
 }
